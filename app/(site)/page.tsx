80db4c28855478c060bf69fca0c9509464837475
Nick Harbaugh <nick@sevenmilemedia.com>
2023-11-14 16:18:37 -0600

    Style and layout updates for mobile

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -15,11 +15,11 @@ export default async function Home() {
   return (
     <div className='mt-5 md:mt-10 lg:mt-20'>
       <h1 className='text-xl leading-normal md:text-2xl lg:text-4xl lg:leading-normal'>Nick Harbaugh is an artist and computer programmer working with data and artificial intelligence to explore what it means to be human in a technological world.</h1>
-      <div className='flex gap-5 mt-5 md:mt-10 lg:mt-20'>
-        <a href='https://www.sevenmilemedia.com/work/' target='_blank' rel='noreferrer' className='underline'>Portfolio</a>
-        <a href='https://www.instagram.com/nickharb/' target='_blank' rel='noreferrer' className='underline'>Instagram</a>
-        <a href='https://www.linkedin.com/in/nickharb/' target='_blank' rel='noreferrer' className='underline'>LinkedIn</a>
-        <a href='https://medium.com/@nickharb' target='_blank' rel='noreferrer' className='underline'>Medium</a>
+      <div className='mt-10 md:flex md:gap-5 lg:mt-20'>
+        <a href='https://www.sevenmilemedia.com/work/' target='_blank' rel='noreferrer' className='block py-1 underline'>Portfolio</a>
+        <a href='https://www.instagram.com/nickharb/' target='_blank' rel='noreferrer' className='block py-1 underline'>Instagram</a>
+        <a href='https://www.linkedin.com/in/nickharb/' target='_blank' rel='noreferrer' className='block py-1 underline'>LinkedIn</a>
+        <a href='https://medium.com/@nickharb' target='_blank' rel='noreferrer' className='block py-1 underline'>Medium</a>
       </div>
     </div>
   )
